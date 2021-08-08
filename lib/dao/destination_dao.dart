5fa8f488a312e97dcc16f455c027573c0fccd2c3
MilitaryIntelligence6 <hlrongzun@qq.com>
2021-08-08 13:02:27 +0800

    重构;

--- lib/dao/destination_dao.dart
+++ lib/dao/destination_dao.dart
@@ -1,32 +1,34 @@
-import 'dart:async';
-import 'dart:convert';
+import "dart:async";
+import "dart:convert";
 
-import 'package:flutter_ctrip/model/destination_model.dart';
-import 'package:http/http.dart' as http;
+import "package:flutter_ctrip/model/destination_model.dart";
+import "package:http/http.dart" as http;
 
 //旅拍搜索接口
-
-const Map<String, Object> Params = {
-  "contentType":"json",
-  "head":{
-    "cid":"09031043410934928682",
-    "ctok":"",
-    "cver":"1.0",
-    "lang":"01",
-    "sid":"8888",
-    "syscode":"09",
-    "auth":"","extension": []
-  },
-  "channel":"H5",
-  "businessUnit":14,
-  "startCity":2
-};
-
-const String Url = "https://sec-m.ctrip.com/restapi/soa2/14422/navigationInfo?_fxpcqlniredt=09031043410934928682";
-
 class DestinationDao {
+
+  DestinationDao._();
+
+  static const Map<String, Object> PARAMS = {
+    "contentType":"json",
+    "head":{
+      "cid":"09031043410934928682",
+      "ctok":"",
+      "cver":"1.0",
+      "lang":"01",
+      "sid":"8888",
+      "syscode":"09",
+      "auth":"","extension": []
+    },
+    "channel":"H5",
+    "businessUnit":14,
+    "startCity":2
+  };
+
+  static const String URL = "https://sec-m.ctrip.com/restapi/soa2/14422/navigationInfo?_fxpcqlniredt=09031043410934928682";
+
   static Future<DestinationModel> fetch() async {
-    final response = await http.post(Url, body: jsonEncode(Params));
+    final response = await http.post(URL, body: jsonEncode(PARAMS));
     if (response.statusCode == 200) {
       // fix 中文乱码;
       Utf8Decoder utf8decoder = Utf8Decoder();
